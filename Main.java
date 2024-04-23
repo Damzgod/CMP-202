ed27f6869c637cdf850847c27fd7779d650614a7
Damzgod <damilolaolorunda7@gmail.com>
2024-04-23 16:18:37 +0100

    first class

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main{
+  public static void main(String[] args) {
+    String name = " Meshach";
+    System.out.println("Hello" + name);
+    int age = 200;
+    System.out.println(name + " is " + age + " years old.");
+    System.out.println(name.length());
+    System.out.println(name.charAt(6));
+  }
+}
