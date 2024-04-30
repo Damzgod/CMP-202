8276597f977de96dbc021ee0286f97db933a2eff
Damzgod <damilolaolorunda7@gmail.com>
2024-04-30 14:20:18 +0100

    OOP

--- Admin.java
+++ Admin.java
@@ -4,7 +4,12 @@ class Admin extends User2{
     String staffNo;
     String uploadResult(String course,float score){
         System.out.println(course + " " + score );
-        return score;
+        return uploadResult(course , score);
+    }
+
+    public static void main(String[] args) {
+        Admin uploadResult = new Admin();
+        System.out.println(uploadResult);
     }
 
 }
--- Student.java
+++ Student.java
@@ -1,6 +1,24 @@
 public class Student extends Admin {
 
+    String matricNo;
+    String AdmissionNo;
 
-    int matricNo;
-    int AdmissionNo;
+    String Registercourse() {
+        return "CMP 202";
+    }
+
+    String CheckResult() {
+        matricNo = "BHU/22/04/05/0009";
+        AdmissionNo = "154096754";
+        if (matricNo.contains("BHU/22/04/05/0009") || AdmissionNo.contains("154096754")) {
+            return "Your result is:";
+        } else {
+            return "Incorrect username or password.";
+        }
+    }
+
+    public static void main(String[] args) {
+        Student CheckResult = new Student();
+        System.out.println(CheckResult.CheckResult());
+    }
 }
